61afd7f6f1436905c756de07ed851cef4caa08f5
fdrgsp <federico.gasparoli@gmail.com>
2025-06-24 17:48:39 -0400

    fix wip

--- update_notebooks.py
+++ update_notebooks.py
@@ -2,6 +2,23 @@ import nbformat
 import sys
 from pathlib import Path
 
+
+map_03_python_II_boke_to_notebook = {
+    "#commenting-printing": "#0.-Commenting-&-Printing",
+    "#data-types": "#1.-Data-Types",
+    "#variables": "#2.-Variables",
+    "#operators": "#3.-Operators",
+    "#data-structures": "#4.-Data-Structures",
+    "#data-structures-lists": "#5.-Data-Structures:-Lists",
+    "#data-structures-tuples": "#6.-Data-Structures:-Tuples",
+    "#data-structures-dictionaries": "#7.-Data-Structures:-Dictionaries",
+    "#data-structures-sets": "#8.-Data-Structures:-Sets",
+    "#for-loops": "#9.-For-Loops",
+    "#if-statements": "#10.-If-Statements",
+    "#functions": "#11.-Functions",
+}
+
+
 def update_notebooks(input_path: str | Path, output_path: str | Path) -> None:
     nb = nbformat.read(input_path, as_version=4)
     cleaned_cells = []
@@ -34,10 +51,19 @@ def update_notebooks(input_path: str | Path, output_path: str | Path) -> None:
             tags.remove("skip-execution")
             cell["metadata"]["tags"] = tags
 
+        # Replace links in intro_to_python_II.ipynb
+        if (
+            Path(input_path).name == "intro_to_python_II.ipynb"
+            and cell.cell_type == "markdown"
+        ):
+            for old_link, new_link in map_03_python_II_boke_to_notebook.items():
+                cell.source = cell.source.replace(old_link, new_link)
+
         cleaned_cells.append(cell)
 
     nb.cells = cleaned_cells
     nbformat.write(nb, output_path)
+
 
 if __name__ == "__main__":
     src = Path(sys.argv[1])
