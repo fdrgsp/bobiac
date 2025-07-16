876f1518ae34b207e3d11a0be54cf35d1ef16522
Antoine A. Ruzette <antoine.ruzette@gmail.com>
2025-07-16 12:54:05 -0400

    student group work

---
+++ student_group_work/evaluate_instance_seg_no_post.py
@@ -0,0 +1,82 @@
+import numpy as np
+import skimage.io
+import skimage.measure
+import scipy.optimize
+import pandas as pd
+import sys
+
+def evaluate_instance_segmentation(pred_path, gt_path="../_static/images/student_group/001_masks.png", iou_threshold=0.5):
+    pred = skimage.io.imread(pred_path)
+    gt = skimage.io.imread(gt_path)
+
+    pred_labels = np.unique(pred[pred > 0])
+    gt_labels = np.unique(gt[gt > 0])
+
+    num_pred = len(pred_labels)
+    num_gt = len(gt_labels)
+
+    TP_px = np.logical_and(pred > 0, gt > 0).sum()
+    FP_px = np.logical_and(pred > 0, gt == 0).sum()
+    FN_px = np.logical_and(pred == 0, gt > 0).sum()
+    TN_px = np.logical_and(pred == 0, gt == 0).sum()
+
+    pixel_accuracy = (TP_px + TN_px) / (TP_px + FP_px + FN_px + TN_px)
+    pixel_f1 = (2 * TP_px) / (2 * TP_px + FP_px + FN_px) if (2 * TP_px + FP_px + FN_px) > 0 else 0
+
+    iou_matrix = np.zeros((num_gt, num_pred))
+    for i, gt_id in enumerate(gt_labels):
+        gt_mask = gt == gt_id
+        for j, pred_id in enumerate(pred_labels):
+            pred_mask = pred == pred_id
+            intersection = np.logical_and(gt_mask, pred_mask).sum()
+            union = np.logical_or(gt_mask, pred_mask).sum()
+            if union > 0:
+                iou_matrix[i, j] = intersection / union
+
+    matched_gt, matched_pred = scipy.optimize.linear_sum_assignment(-iou_matrix)
+    matches = [(i, j) for i, j in zip(matched_gt, matched_pred) if iou_matrix[i, j] >= iou_threshold]
+
+    tp = len(matches)
+    fp = num_pred - tp
+    fn = num_gt - tp
+    f1_instances = (2 * tp) / (2 * tp + fp + fn) if (2 * tp + fp + fn) > 0 else 0
+    miou = np.mean([iou_matrix[i, j] for (i, j) in matches]) if matches else 0
+
+    gt_props = {r.label: r.area for r in skimage.measure.regionprops(gt)}
+    pred_props = {r.label: r.area for r in skimage.measure.regionprops(pred)}
+    area_errors = []
+    for i, j in matches:
+        gt_area = gt_props[gt_labels[i]]
+        pred_area = pred_props[pred_labels[j]]
+        abs_error = abs(gt_area - pred_area)
+        rel_error = abs_error / gt_area if gt_area > 0 else 0
+        area_errors.append((gt_area, pred_area, abs_error, rel_error))
+
+    avg_abs_area_error = np.mean([ae[2] for ae in area_errors]) if area_errors else 0
+    avg_rel_area_error = np.mean([ae[3] for ae in area_errors]) if area_errors else 0
+
+    metrics_table = pd.DataFrame({
+        "Metric": [
+            "Pixel Accuracy", "Pixel F1 Score",
+            "Instance F1 Score", "Mean IoU",
+            "Avg. Abs Area Error", "Avg. Rel Area Error",
+            "TP Instances", "FP Instances", "FN Instances"
+        ],
+        "Value": [
+            round(pixel_accuracy, 4), round(pixel_f1, 4),
+            round(f1_instances, 4), round(miou, 4),
+            round(avg_abs_area_error, 2), round(avg_rel_area_error, 4),
+            tp, fp, fn
+        ]
+    })
+
+    print(metrics_table.to_string(index=False))
+    return metrics_table
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: python evaluate_instance_seg_no_post.py <prediction_mask_path>")
+        sys.exit(1)
+    
+    pred_path = sys.argv[1]
+    evaluate_instance_segmentation(pred_path)
--- student_group_work/student_group_work.md
+++ student_group_work/student_group_work.md
@@ -1,3 +1,58 @@
 # <i class="fa-solid fa-user-group"></i> Student Group Work
 
-Here we need to add info about how the group work is structured, what are the goals, the logistics, the output expectation and the detail protocol they need to follow and execute.
+## Overview
+
+Your turn! The goal of this group work is to apply the knowledge you have acquired in the course to develop a cell (instance) segmentation pipeline on 2D images provided.
+
+## Data
+
+<a href="../_static/data/student_group_work/001_img.tiff" download> <i class="fas fa-download"></i> Student Group Work Image </a>
+
+## Submission
+
+To submit your results, use the following notebook: [Evaluation notebook](../../notebooks/student_group_work/evaluation-notebook.ipynb)
+
+
+## Leaderboard
+
+The leaderboard is available at [BoBIAC 2025 Segmentation Leaderboard](https://liveboard-bobiac.onrender.com/).
+
+## Evaluation metrics
+
+Evaluating segmentation pipelines is a complex task, and there is no single metric that can capture all the aspects of the performance. We have chosen to use the following metrics:
+
+- **Precision**: The precision is the percentage of pixels that are correctly classified.
+
+$$
+\text{Precision} = \frac{\text{Number of correctly classified pixels}}{\text{Total number of pixels}}
+$$
+
+- **Recall**: The recall is the percentage of pixels that are correctly classified.
+
+$$
+\text{Recall} = \frac{\text{Number of correctly classified pixels}}{\text{Total number of pixels in the ground truth}}
+$$
+
+- **Intersection over Union (IoU)**: The intersection over union is the ratio of the intersection of the predicted and ground truth masks to the union of the two masks.
+
+$$
+\text{IoU} = \frac{\text{Intersection}}{\text{Union}}
+$$
+
+- **Pixel accuracy**: The pixel accuracy is the percentage of pixels that are correctly classified.
+
+$$
+\text{Pixel accuracy} = \frac{\text{Number of correctly classified pixels}}{\text{Total number of pixels}}
+$$
+
+- **Mean Intersection over Union (mIoU)**: The mean intersection over union is the average of the intersection over union for each class.
+
+$$
+\text{mIoU} = \frac{1}{n} \sum_{i=1}^{n} \frac{\text{Intersection}_i}{\text{Union}_i}
+$$
+
+- **Pixel F1 score**: The pixel F1 score is the harmonic mean of the precision and recall.
+
+$$
+\text{Pixel F1 score} = 2 \times \frac{\text{Precision} \times \text{Recall}}{\text{Precision} + \text{Recall}}
+$$
